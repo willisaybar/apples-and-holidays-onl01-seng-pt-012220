3298931bec6682c4c02ee837bbe02ba98c4616ab
Willis Aybar <willisaybar@gmail.com>
2020-02-03 23:32:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -13,9 +13,9 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # holiday_hash[:summer][:fourth_of_july][1]
-  # holiday_hash[:winter][:christmas] = "Balloons"
-  # holiday_hash[:winter][:new_years] = "Balloons"
+    holiday_hash[:summer][:fourth_of_july][1]
+    holiday_hash[:winter][:christmas] = "Balloons"
+    holiday_hash[:winter][:new_years] = "Balloons"
     holiday_hash[:spring][:memorial_day] = "Grill"
     holiday_hash[:spring][:memorial_day]= "Table Cloth"
 end
