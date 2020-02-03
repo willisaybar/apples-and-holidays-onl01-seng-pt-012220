4a783733036563cec9ef456b0a4c8a5be7a567ba
Willis Aybar <willisaybar@gmail.com>
2020-02-03 23:11:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -4,30 +4,15 @@ def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
   end
 
-# given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
-
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:summer][:fourth_of_july][1]
+  holiday_hash[:winter][:christmas],[:new_years][]
+
+end
+
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
-end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
