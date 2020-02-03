8c1b830f54366885b7728c2c00a942dbb7094dcb
Willis Aybar <willisaybar@gmail.com>
2020-02-03 23:15:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -11,16 +11,17 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 
 end
 
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+  holiday_hash[:summer][:fourth_of_july][1]
+  holiday_hash[:winter][:christmas] = "Balloons"
+  holiday_hash[:winter][:new_years] = "Balloons"
+  holiday_hash[:spring][:memorial_day] = "Grill"
 
 end
+
+ # again, holiday_hash is the same as the ones above
+  # add the second argument to the memorial day array
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
