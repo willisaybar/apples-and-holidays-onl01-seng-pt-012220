10084e9b81d395f4ca32d7ba3dc660dded3b31cb
Willis Aybar <willisaybar@gmail.com>
2020-02-03 23:06:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
+  holiday_hash[:summer][:fourth_of_july][1]
   # given that holiday_hash looks like this:
   # {
   #   :winter => {
@@ -19,6 +20,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # }
   # return the second element in the 4th of July array
   
+end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
